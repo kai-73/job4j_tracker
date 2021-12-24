54ce2fcf5d264764fb6ad62510b2bfb8e44205ff
kai-73 <oroman@rambler.ru>
2021-12-24 13:05:09 +0300

    1.8. Состояние объекта [#167388 #242715]

--- src/test/java/ru/job4j/oop/PointTest.java
+++ src/test/java/ru/job4j/oop/PointTest.java
@@ -1,9 +1,43 @@
 package ru.job4j.oop;
 
-import junit.framework.TestCase;
+import org.junit.Assert;
+import org.junit.Test;
 
-public class PointTest extends TestCase {
+public class PointTest {
 
-    public void testDistance() {
+    @Test
+    public void when00to20then2() {
+        Point a = new Point(0, 0);
+        Point b = new Point(0, 2);
+        double expected = 2;
+        double out = a.distance(b);
+        Assert.assertEquals(expected, out, 0.01);
+    }
+
+    @Test
+    public void when03to25then2point82() {
+        Point a = new Point(0, 3);
+        Point b = new Point(2, 5);
+        double expected = 2.82;
+        double out = a.distance(b);
+        Assert.assertEquals(expected, out, 0.01);
+    }
+
+    @Test
+    public void when01to23then2point82() {
+        Point a = new Point(0, 1);
+        Point b = new Point(2, 3);
+        double expected = 2.82;
+        double out = a.distance(b);
+        Assert.assertEquals(expected, out, 0.01);
+    }
+
+    @Test
+    public void when01to67then8point48() {
+        Point a = new Point(0, 1);
+        Point b = new Point(6, 7);
+        double expected = 8.48;
+        double out = a.distance(b);
+        Assert.assertEquals(expected, out, 0.01);
     }
 }
