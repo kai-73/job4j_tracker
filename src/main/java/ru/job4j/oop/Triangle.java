8a30cb3aa58c049604adcfc6fcf0e09667b1dc1c
kai-73 <oroman@rambler.ru>
2021-12-24 19:10:36 +0300

    2.2. Рефакторинг - Площадь треугольника. [#122642]

---
+++ src/main/java/ru/job4j/oop/Triangle.java
@@ -0,0 +1,34 @@
+package ru.job4j.oop;
+
+public class Triangle {
+    private Point first;
+    private Point second;
+    private Point third;
+
+    public Triangle(Point ap, Point bp, Point cp) {
+        this.first = ap;
+        this.second = bp;
+        this.third = cp;
+    }
+
+    public double semiPerimetr(double a, double b, double c) {
+        return (a + b + c) / 2;
+    }
+
+    public boolean exist(double ab, double ac, double bc) {
+        return ab + ac > bc && ac + bc > ab && ab + bc > ac;
+    }
+
+    public double area() {
+        double result = -1;
+        double ab = first.distance(second);
+        double ac = first.distance(third);
+        double bc = second.distance(third);
+        System.out.println("ab: " + ab + ", ac: " + ac + ", bc: " + bc);
+        if (this.exist(ab, ac, bc)) {
+            double per = semiPerimetr(ab, ac, bc);
+            return Math.sqrt(per * (per - ab) * (per - ab) * (per - bc));
+        }
+        return result;
+    }
+}
---
+++ src/test/java/ru/job4j/oop/TringleTest.java
@@ -0,0 +1,40 @@
+package ru.job4j.oop;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class TringleTest {
+
+    @Test
+    public void areaPointA00PointB40PointC04() {
+        Point a = new Point(0, 0);
+        Point b = new Point(4, 0);
+        Point c = new Point(0, 4);
+        Triangle triangle = new Triangle(a, b, c);
+        double rsl = triangle.area();
+        double expected = 8;
+        Assert.assertEquals(expected, rsl, 0.01);
+    }
+
+    @Test
+    public void areaPointA00PointB04PointC40() {
+        Point a = new Point(0, 0);
+        Point b = new Point(0, 4);
+        Point c = new Point(4, 0);
+        Triangle triangle = new Triangle(a, b, c);
+        double rsl = triangle.area();
+        double expected = 8;
+        Assert.assertEquals(expected, rsl, 0.01);
+    }
+
+    @Test
+    public void areaPointA00PointB02PointC23() {
+        Point a = new Point(0, 0);
+        Point b = new Point(0, 2);
+        Point c = new Point(2, 2);
+        Triangle triangle = new Triangle(a, b, c);
+        double rsl = triangle.area();
+        double expected = 3.11;
+        Assert.assertEquals(expected, rsl, 0.01);
+    }
+}
