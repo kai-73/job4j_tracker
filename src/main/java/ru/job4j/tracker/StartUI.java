99892649e884e33d2ac2f37752fb40db8aea5cb8
kai-73 <oroman@rambler.ru>
2021-12-31 01:27:43 +0300

    6. Date. Отображение даты. [#310017]

---
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -0,0 +1,15 @@
+package ru.job4j.tracker;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class StartUI {
+
+    public static void main(String[] args) {
+        Item dateTime = new Item();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss");
+        String outPut =  dateTime.getCreated().format(formatter);
+        System.out.println(outPut);
+    }
+
+}
