253279dc59149a114aa5b6bb0753354fa9cacb43
kai-73 <oroman@rambler.ru>
2022-01-03 15:16:02 +0300

    примеры из учебника

---
+++ src/main/java/ru/job4j/primeri/Circle.java
@@ -0,0 +1,19 @@
+package ru.job4j.primeri;
+
+public class Circle {
+    private double radius;
+    private String color;
+
+    public Circle(double r) {
+        radius = r;
+        color = "красный";
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public String getColor() {
+        return color;
+    }
+}
---
+++ src/main/java/ru/job4j/primeri/CircleTest.java
@@ -0,0 +1,8 @@
+package ru.job4j.primeri;
+
+public class CircleTest {
+    public static void main(String[] args) {
+        Circle c1 = new Circle(2.0);
+        System.out.println("Радиус= " + c1.getRadius() + "Цвет- " + c1.getColor());
+    }
+}
