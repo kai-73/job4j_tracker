a47d3eb3e650c2c5b6d7b1c8b654952683e0e0f4
kai-73 <oroman@rambler.ru>
2022-01-04 14:39:10 +0300

    3.1. Массив с пустыми ячейками [#176470]

---
+++ src/main/java/ru/job4j/pojo/Shop.java
@@ -0,0 +1,22 @@
+package ru.job4j.pojo;
+
+public class Shop {
+
+    public static int indexOfNull(Product[] products) {
+        for (int i = 0; i < products.length; i++) {
+            Product pr = products[i];
+            if (pr == null) {
+                return i;
+            }
+
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        Product[] products = new Product[5];
+        products[0] = new Product("Milk", 10);
+        products[1] = new Product("Bread", 4);
+        products[2] = new Product("Egg", 19);
+    }
+}
---
+++ src/test/java/ru/job4j/pojo/ShopTest.java
@@ -0,0 +1,33 @@
+package ru.job4j.pojo;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class ShopTest {
+    @Test
+    public void whenLastNull() {
+        Product[] products = new Product[5];
+        products[0] = new Product("Milk", 10);
+        products[1] = new Product("Bread", 4);
+        products[2] = new Product("Egg", 19);
+        int rsl = Shop.indexOfNull(products);
+        assertEquals(rsl, 3);
+    }
+
+    @Test
+    public void whenFirstNull() {
+        Product[] products = new Product[5];
+        products[1] = new Product("Milk", 10);
+        int rsl = Shop.indexOfNull(products);
+        assertEquals(rsl, 0);
+    }
+
+    @Test
+    public void whenHasNotNull() {
+        Product[] products = new Product[1];
+        products[0] = new Product("Milk", 10);
+        int rsl = Shop.indexOfNull(products);
+        assertEquals(rsl, -1);
+    }
+}
