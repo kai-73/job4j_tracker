8986714291cc5173e8f8b188f750c922e4593bb9
kai-73 <oroman@rambler.ru>
2022-01-07 16:33:35 +0300

    5. Tracker - хранилище [#396]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -1,9 +1,22 @@
 package ru.job4j.tracker;
 
+import java.util.Arrays;
+import java.util.Objects;
+
 public class Tracker {
+    /**
+     * items - массив для хранения заявок.
+     */
     private final Item[] items = new Item[100];
     private int ids = 1;
     private int size = 0;
+
+    /**
+     * В методе add реализована процедура добавления новой заявки
+     * в массив, где хранятся все заявки.
+     * @param item - аргумент метода- на вход метода поступает объект , т.е. новая заявка.
+     * @return возвращает объект с присвоенным ключом "ads"
+     */
 
     public Item add(Item item) {
         item.setId(ids++);
@@ -11,6 +24,11 @@ public class Tracker {
         return item;
     }
 
+    /**
+     * Метод реализует поиск заявки по id.
+     * @param id - это id заявки.
+     * @return - возвращает объект - заявку.
+     */
     public Item findById(int id) {
         Item rsl = null;
         for (int index = 0; index < size; index++) {
@@ -21,5 +39,40 @@ public class Tracker {
             }
         }
         return rsl;
+    }
+
+    /**
+     * Метод ищет null элементы в массиве items, создает массив tmp и помещает
+     * в него все не null элементы.
+     * @return Item[].
+     */
+
+    public Item[] findAll() {
+        Item[] tmp = new Item[size];
+        int count = 0;
+        for (int i = 0; i < size; i++) {
+            if (items[i] != null) {
+                tmp[count++] = items[i];
+            }
+        }
+        return Arrays.copyOf(tmp, count);
+    }
+
+    /**
+     * Метод ищет элеменнты массива items, совпадающие с "key" и помещает их в отдельный
+     * массив.
+     * @param key Имя в искомой заявке
+     * @return Item[]
+     */
+
+    public Item[] findByName(String key) {
+        Item[] rsl = new Item[size];
+        int count = 0;
+        for (int i = 0; i < size; i++) {
+            if (items[i].getName().equals(key)) {
+                rsl[count++] = items[i];
+            }
+        }
+        return Arrays.copyOf(rsl, count);
     }
 }
