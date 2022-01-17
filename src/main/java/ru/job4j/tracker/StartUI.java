52b11b39b9791d46d53a21c3cea4f4d3ee391e8e
kai-73 <oroman@rambler.ru>
2022-01-17 13:50:36 +0300

    4.2. Статические методы. [#181779]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -1,7 +1,6 @@
 package ru.job4j.tracker;
 
 public class StartUI {
-    //private boolean run;
 
     public static void createItem(Input input, Tracker tracker) {
         System.out.println(" === Create a new Item === ");
