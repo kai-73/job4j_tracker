d82307034e57b0e5154ce9a2b7903b42015cf600
kai-73 <oroman@rambler.ru>
2022-02-07 19:23:25 +0300

    2.1. Тест на ValidateInput [#311887 #261226]

--- src/test/java/ru/job4j/tracker/ValidateInputTest.java
+++ src/test/java/ru/job4j/tracker/ValidateInputTest.java
@@ -2,16 +2,50 @@ package ru.job4j.tracker;
 
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 public class ValidateInputTest {
 
     @Test
     public void whenInvalidInput() {
         Output output = new StubOutput();
-        Input in = new StubInput(new String[]{"Oleg", "1"});
+        String[] answers = new String[]{"Oleg", "1"};
+        Input in = new StubInput(answers);
         ValidateInput input = new ValidateInput(output, in);
         int selected = input.askInt("Все равно что");
         assertEquals(1, selected);
+    }
+
+    @Test
+    public void whenInvalidInputSingleCorrect() {
+        Output output = new StubOutput();
+        String[] answers = new String[]{"1"};
+        Input in = new StubInput(answers);
+        ValidateInput input = new ValidateInput(output, in);
+        int selected = input.askInt("Все равно что");
+        assertEquals(1, selected);
+    }
+
+    @Test
+    public void whenInvalidInputMutipleCorrect() {
+        Output output = new StubOutput();
+        String[] answers = new String[]{"0", "1", "3", "10"};
+        Input in = new StubInput(answers);
+        ValidateInput input = new ValidateInput(output, in);
+        int[] selected = new int[answers.length];
+        for (int i = 0; i < selected.length; i++) {
+            selected[i] = input.askInt("Enter menu:");
+            assertEquals(Integer.parseInt(answers[i]), selected[i]);
+        }
+    }
+
+    @Test
+    public void whenInvalidInputNegativeNumber() {
+        Output output = new StubOutput();
+        String[] answers = new String[]{"-1"};
+        Input in = new StubInput(answers);
+        ValidateInput input = new ValidateInput(output, in);
+        int selected = input.askInt("Все равно что");
+        assertEquals(-1, selected);
     }
 }
