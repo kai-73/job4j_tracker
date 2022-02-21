dfcf26c8ce6b7f71fed7e1eebea53155c60a6d3a
kai-73 <oroman@rambler.ru>
2022-02-21 16:33:57 +0300

    1. Телефонный справочник на базе ArrayList [#41598]

---
+++ src/main/java/ru/job4j/search/PhoneDictionary.java
@@ -0,0 +1,30 @@
+package ru.job4j.search;
+
+import java.util.ArrayList;
+
+public class PhoneDictionary {
+    private ArrayList<Person> persons = new ArrayList<>();
+
+    public void add(Person person) {
+        this.persons.add(person);
+    }
+
+    /**
+     * Вернуть список всех пользователей, который содержат key в любых полях.
+     * @param key Ключ поиска.
+     * @return Список подощедщих пользователей.
+     */
+    public ArrayList<Person> find(String key) {
+        ArrayList<Person> result = new ArrayList<>();
+        //if (persons.contains(key)) {
+        //    result.add(element);
+         //   }
+        for (Person element : persons) {
+            if (element.getName().contains(key) || element.getSurname().contains(key)
+                    || element.getPhone().contains(key) || element.getAddress().contains(key)) {
+                result.add(element);
+            }
+        }
+        return result;
+    }
+}
---
+++ src/test/java/ru/job4j/search/PhoneDictionaryTest.java
@@ -0,0 +1,28 @@
+package ru.job4j.search;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+public class PhoneDictionaryTest {
+
+    @Test
+    public void whenFindByName() {
+        PhoneDictionary phones = new PhoneDictionary();
+        phones.add(new Person("Petr", "Arsentev", "534872", "Bryansk"));
+        ArrayList<Person> persons = phones.find("Petr");
+        assertEquals("Arsentev", persons.get(0).getSurname());
+    }
+
+    @Test
+    public void whenFindByNameNull() {
+        PhoneDictionary phones = new PhoneDictionary();
+        phones.add(new Person("Petr", "Arsentev", "534872", "Bryansk"));
+        ArrayList<Person> persons = phones.find("Oleg");
+        assertTrue(persons.isEmpty());
+
+    }
+}
